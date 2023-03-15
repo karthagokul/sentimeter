bff74fad84957c06372da4410452b81c11b150e3
Gokul Kartha <gokul.kartha@bolgatty.nl>
2023-03-15 20:57:36 +0100

    Integrated logging

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import signal
 import sys
-from sentimeter import Sentimeter
 import click
+import logging
+logging.basicConfig(filename='app.log', filemode='w', level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s')
 
+from sentimeter import Sentimeter
 app = Sentimeter()
-
 
 @click.command()
 @click.option(
@@ -17,22 +18,24 @@ def main(live, text, audio_file, text_file):
     signal.signal(signal.SIGINT, signal_handler)
     global app
     if live == True:
-        print("Live")
+        logging.info('Started Listening Mode')
         app.start_listening()
     elif text != None:
-        click.echo("Hello %s!" % text)
+        pass
         #app.process_text(text)
     elif audio_file != None:
-        print("Process audio file")
+        logging.info("Process audio file")
         app.process_audio_file(audio_file)
     elif text_file != None:
-        print("Process text file")
+        logging.info("Process text file")
+    else:
+        logging.fatal("Unable to Recognize")
     return
 
 
 def signal_handler(sig, frame):
     #global app
-    print("Please wait, Let me cleanup")
+    logging.info("Please wait, Let me cleanup")
     app.stop()
     sys.exit(0)
 
